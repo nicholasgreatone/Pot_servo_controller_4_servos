40330936e4918b8faee5617e53749537f71b4cbd
nicholasgreatone <47575898+nicholasgreatone@users.noreply.github.com>
2023-03-10 19:05:00 -0500

    Update Four_servos_control_V1.ino

--- Four_servos_control_V1.ino
+++ Four_servos_control_V1.ino
@@ -1,16 +1,11 @@
 #include <Servo.h>
 
-
-//**** button servo settings
-
-
-
 //**** servo 1 settings
 Servo servo1;
 
 const int servo1PotPin = A0;
 
-const int servo1Pin = 5; // Must use PWM enabled pin
+const int servo1Pin = 5; //PWM enabled pin
 
 int servo1_test;
 
@@ -22,7 +17,7 @@ Servo servo2;
 
 const int servo2PotPin = A1;
 
-const int servo2Pin = 3; // Must use PWM enabled pin
+const int servo2Pin = 3; // PWM enabled pin
 
 int servo2_test;
 
@@ -35,7 +30,7 @@ Servo servo3;
 
 const int servo3PotPin = A2;
 
-const int servo3Pin = 6; // Must use PWM enabled pin
+const int servo3Pin = 6; // PWM enabled pin
 
 int servo3_test;
 
@@ -46,7 +41,7 @@ Servo servo4;
 
 const int servo4PotPin = A3;
 
-const int servo4Pin = 9; // Must use PWM enabled pin
+const int servo4Pin = 9; // PWM enabled pin
 
 int servo4_test;
 
@@ -69,25 +64,25 @@ void loop() {
 
 servo1_test = analogRead(servo1PotPin);
 
-servo1_test = map(servo1_test, 0, 1023, 0, 180); //servo rotation is only 65 degrees. currently translating potentiometer values to degrees of rotation for servo, currently in reverse
+servo1_test = map(servo1_test, 0, 1023, 0, 180); // potentiometer to rotation for servo
 
 servo1.write(servo1_test);
 
 servo2_test = analogRead(servo2PotPin);
 
-servo2_test = map(servo2_test,  0, 1023, 0, 180); //servo rotation is only 80 degrees. currently translating potentiometer values to degrees of rotation for servo, currently in reverse
+servo2_test = map(servo2_test,  0, 1023, 0, 180); // potentiometer to rotation for servo
 
 servo2.write(servo2_test);
 
 servo3_test = analogRead(servo3PotPin);
 
-servo3_test = map(servo3_test, 0, 1023, 0, 180); //servo rotation is only 65 degrees. currently translating potentiometer values to degrees of rotation for servo, currently in reverse
+servo3_test = map(servo3_test, 0, 1023, 0, 180); // potentiometer to rotation for servo
 
 servo3.write(servo3_test);
 
 servo4_test = analogRead(servo4PotPin);
 
-servo4_test = map(servo4_test, 0, 1023, 0, 180); //servo rotation is only 65 degrees. currently translating potentiometer values to degrees of rotation for servo, currently in reverse
+servo4_test = map(servo4_test, 0, 1023, 0, 180); // potentiometer to rotation for servo
 
 servo4.write(servo4_test);
 
